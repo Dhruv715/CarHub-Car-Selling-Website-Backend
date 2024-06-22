9620ff61274d01b2fe2a87c40c6336309ab7f4e7
dhruv <druvzanzmera@gmail.com>
2024-06-22 22:41:30 +0530

    Database Live MongoDB Atlas

--- app.js
+++ app.js
@@ -49,9 +49,9 @@ app.use(function(err, req, res, next) {
 });
 
 // Listen on port 5000
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
 
 module.exports = app;
